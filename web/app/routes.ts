166a845a1e6e282af53e1323c7a23d605a804625
Daichi343 <naka.d0114nnp@gmail.com>
2025-05-18 00:38:56 +0900

    routes.tsもコミット

--- web/app/routes.ts
+++ web/app/routes.ts
@@ -1,8 +1,9 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/interimresult1.tsx"),
-  // route("interimresult1", "./routes/interimresult1.tsx"), // ラウンド1の中間発表
+  index("routes/answercheck.tsx"),
+  route("interimresult1", "./routes/interimresult1.tsx"), // ラウンド1の中間発表
+  // route("answercheck", "./routes/answercheck.tsx"), // 解答確認（マージしてない）
   route("interimresult2", "./routes/interimresult2.tsx"), // ラウンド2の中間発表
   route("finalresult", "./routes/finalresult.tsx"), // 最終結果発表
   route("roundmasterfirst", "./routes/roundmasterfirst.tsx"),
