1dfdbbc0b4303b3cc2bf8173e756a1df733f9674
yashikota <52403688+yashikota@users.noreply.github.com>
2025-11-06 01:51:07 +0900

    air

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,16 +2,71 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/yashikota/scene-hunter/server/internal/domain/room"
-	"github.com/yashikota/scene-hunter/server/internal/domain/user"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	slogchi "github.com/samber/slog-chi"
 )
 
 func main() {
-	u := user.NewUser("user-code", "user-name")
-	log.Println(u)
+	// Logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	config := slogchi.Config{
+		WithSpanID:  true,
+		WithTraceID: true,
+	}
+	router := chi.NewRouter()
+	router.Use(slogchi.NewWithConfig(logger, config))
+	router.Use(middleware.Recoverer)
 
-	r := room.NewRoom("room-code")
-	log.Println(r)
+	// Health check endpoint
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(time.Now().Format(time.RFC3339)))
+	})
+
+	// Create server
+	srv := &http.Server{
+		Addr:         ":8686",
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	// Start server in a goroutine
+	go func() {
+		log.Printf("Server starting on http://localhost:%s", srv.Addr)
+		err := srv.ListenAndServe()
+
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	// Graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exited")
 }
--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,13 @@ go 1.25.0
 
 require (
 	connectrpc.com/connect v1.19.1
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/google/uuid v1.6.0
 	google.golang.org/protobuf v1.36.10
+)
+
+require (
+	github.com/samber/slog-chi v1.17.0 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
 )
